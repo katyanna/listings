0e2b1187fc1232e43fd2654d95b11e710f1cd606
Katyanna Moura <amelie.kn@gmail.com>
2017-03-14 11:48:48 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask import request, render_template
+import jinja2
+import pdb
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/')
+def get():
+    items = request.args.getlist("food")
+    return render_template("index.html", items = items)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
